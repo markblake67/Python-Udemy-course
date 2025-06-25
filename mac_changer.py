e43c1a85dc2179baacba08136b5a2764d0a78ad8
markblake67 <163885401+markblake67@users.noreply.github.com>
2025-06-25 18:25:32 -0500

    Add files via upload

---
+++ mac_changer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import subprocess
+
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:88", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call("ifconfig", shell=True)

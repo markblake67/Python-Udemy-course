25e8e7b19fe9848d143af5102c0446130624db62
markblake67 <163885401+markblake67@users.noreply.github.com>
2025-06-25 19:08:47 -0500

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -2,7 +2,13 @@
 
 import subprocess
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:88", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
-subprocess.call("ifconfig", shell=True)
+interface = "eth0"
+new_mac = "12:11:22:33:44:00" #New MAC has to start with even octet
+
+print(f"[+] Changing MAC address for {interface} to {new_mac}") #Test print
+
+#All these commands can be run command?
+subprocess.call(f"ifconfig {interface} down", shell=True)  #sets eth0 the ethernet interface to down, need to do before changing MAC. Can also be run
+subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True) #sets the MAC address (found in 'ether' section to this number. Tells the shell to run as True
+subprocess.call(f"ifconfig {interface} up", shell=True) #puts eth0 back up.
+subprocess.call(f"ifconfig", shell=True) #runs ifconfig again to show changed MAC.
